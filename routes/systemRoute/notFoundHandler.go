a7c000e06c24bf885181856edf1183e4bae0ee12
Mgen <m@mgenware.com>
2018-05-28 20:37:10 +0800

    Fix multiple writeHeader calls

--- routes/systemRoute/notFoundHandler.go
+++ routes/systemRoute/notFoundHandler.go
@@ -6,7 +6,6 @@ import (
 )
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
 	msg := "The resource you requested \"" + r.URL.String() + "\" does not exist."
 
 	w.WriteHeader(http.StatusNotFound)

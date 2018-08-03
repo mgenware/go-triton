3f50d36efc5e1b567096ca4df7c3c7b7b22a78d7
Mgen <m@mgenware.com>
2018-08-03 23:48:15 +0800

    Use FormatLocalizedString to quick format localized strings

--- handlers/system/notFoundHandler.go
+++ handlers/system/notFoundHandler.go
@@ -1,7 +1,6 @@
 package system
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/mgenware/go-triton/app"
@@ -10,7 +9,7 @@ import (
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	tm := app.TemplateManager
 	ctx := r.Context()
-	msg := fmt.Sprintf(tm.LocalizationManager.ValueForKey(ctx, "pPageNotFound"), r.URL.String())
+	msg := tm.FormatLocalizedString(ctx, "pPageNotFound", r.URL.String())
 
 	resp := tm.NewHTMLResponse(ctx, w)
 	resp.MustFailWithMessage(msg)

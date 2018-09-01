69f6684e771963465875994457e8c2c0c12965ac
Mgen <m@mgenware.com>
2018-09-01 16:44:24 +0800

    Let error throw in dev mode

--- app/template/manager.go
+++ app/template/manager.go
@@ -64,6 +64,14 @@ func (m *Manager) MustComplete(lang string, d *MasterPageData, w http.ResponseWr
 
 // MustError executes the main view template with the specified data and panics if error occurs.
 func (m *Manager) MustError(lang string, d *ErrorPageData, w http.ResponseWriter) {
+	if m.devMode && !d.Expected {
+		fmt.Println("🙉 This message only shows in dev mode.")
+		if d.Error != nil {
+			panic(d.Error)
+		} else {
+			panic(d.Message)
+		}
+	}
 	errorHTML := m.errorView.MustExecuteToString(lang, d)
 	htmlData := NewMasterPageData("Error", errorHTML)
 	m.MustComplete(lang, htmlData, w)

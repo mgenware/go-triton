db74a45bdc91595a6e80da94ad8eab2d8ebfb078
Mgen <m@mgenware.com>
2018-09-01 16:52:24 +0800

    Document ErrorPageData.Expected

--- app/template/errorPageData.go
+++ app/template/errorPageData.go
@@ -3,7 +3,9 @@ package template
 // ErrorPageData contains information about an error.
 type ErrorPageData struct {
 	LocalizedTemplateData
-	Error    error
-	Message  string
+	Error   error
+	Message string
+
+	// Expected errors don't trigger panic() in dev mode.
 	Expected bool
 }

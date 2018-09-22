6e7b201e2040af912f65e20b8df41bee909b35fa
Mgen <m@mgenware.com>
2018-09-23 00:27:07 +0800

    Add omitempty attribute to code field

--- app/template/apiResult.go
+++ app/template/apiResult.go
@@ -3,7 +3,7 @@ package template
 // APIResult contains the information about the return value of an API, which is either success or failure.
 type APIResult struct {
 	// Code indicates the status code of this result. 0 means success.
-	Code uint `json:"code"`
+	Code uint `json:"code,omitempty"`
 
 	// Message represents an additional string message alongside the result. For example, in a error result, it could be the error message, and when `Code` is 0 (indicating a success result), it could be a friendly message.
 	Message string `json:"message,omitempty"`

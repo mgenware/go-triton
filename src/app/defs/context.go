9abc42f7f0e5bee6e57c354ab2d8f42c43d71b2e
Mgen <m@mgenware.com>
2019-03-06 21:03:59 +0800

    Simplify code in context.go

--- src/app/defs/context.go
+++ src/app/defs/context.go
@@ -6,26 +6,12 @@ import (
 
 // LanguageContext returns the localization language ID associated with the specified context.
 func LanguageContext(ctx context.Context) string {
-	val := ctx.Value(LanguageContextKey)
-	if val == nil {
-		return ""
-	}
-	result, ok := val.(string)
-	if ok {
-		return result
-	}
-	return ""
+	result, _ := ctx.Value(LanguageContextKey).(string)
+	return result
 }
 
 // BodyContext returns the localization language ID associated with the specified context.
 func BodyContext(ctx context.Context) map[string]interface{} {
-	val := ctx.Value(BodyContextKey)
-	if val == nil {
-		return nil
-	}
-	result, ok := val.(map[string]interface{})
-	if ok {
-		return result
-	}
-	return nil
+	result, _ := ctx.Value(BodyContextKey).(map[string]interface{})
+	return result
 }
